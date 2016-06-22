c40b9f0fcc25bf6c5f3b479bac700112a8014293
ZengTao <so.zengtao@gmail.com>
2016-06-22 11:27:57 +0800

    autoload relate files

--- lib/acts_as_followable.rb
+++ lib/acts_as_followable.rb
@@ -1,5 +1,7 @@
 require "acts_as_followable/version"
 
 module ActsAsFollowable
-  
+  autoload :Followable,   'acts_as_followable/followable'
+  # autoload :Follower,   'acts_as_followable/follower'
+  autoload :FollowExt,   'acts_as_followable/follow_ext'
 end

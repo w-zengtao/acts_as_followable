3aefec371ab328cab8b86ca3a8043ea01186f5b4
ZengTao <so.zengtao@gmail.com>
2016-06-22 13:21:47 +0800

    hot fix

--- lib/acts_as_followable.rb
+++ lib/acts_as_followable.rb
@@ -3,7 +3,7 @@ require "acts_as_followable/version"
 module ActsAsFollowable
   autoload :Followable,   'acts_as_followable/followable'
   # autoload :Follower,   'acts_as_followable/follower'
-  autoload :FollowExt,   'acts_as_followable/follow_ext'
+  autoload :FollowableExt,   'acts_as_followable/follow_ext'
 
   require 'acts_as_followable/railtie' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 end
